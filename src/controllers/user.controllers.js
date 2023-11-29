cfe3d83042053ebaa46483dc36ab427cd5532b34
aisarthak <84112158+cooldevloper143@users.noreply.github.com>
2023-11-29 21:59:42 +0530

    000

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,11 +4,14 @@ import asyncHandler from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req, res, next) => {
-    console.log("req.body", req.body);
-    res.status(200).json({
-        message: "Register user route",
-        success: true
-    });
+    // get user details form frontend
+    // validation  - not empty
+    // check if user already exists: username email
+    // check for images , check for avatar 
+    // upload them to cloudinary , avatar
+    // create user object create entry in db 
+
+
 })
 
 // login controllers
